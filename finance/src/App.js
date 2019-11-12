138dafd75f23de9eb712b14d0f879405da84bcf2
Carol254 <paulomugoo@gmail.com>
2019-11-12 09:20:43 +0300

    all the modified files origin master

--- finance/src/App.js
+++ finance/src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 
-import Ninjas from "./Ninja";
+import Ninjas from "./Ninjas";
+import AddNinja from "./AddNinja";
 
 class App extends Component {
   state = {
     ninjas: [
-      { name: "Ryu", age: 45, belt: "black", id: 1 },
-      { name: "Yoshi", age: 34, belt: "green", id: 2 },
-      { name: "crystal", age: 45, belt: "green", id: 3 }
+      { name: "Ryu", age: 45, belt: "black", gender: "female", id: 1 },
+      { name: "Yoshi", age: 34, belt: "green", gender: "male", id: 2 },
+      { name: "crystal", age: 45, belt: "green", gender: "female", id: 3 }
     ]
+  };
+  addNinja = ninja => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    });
+  };
+
+  deleteNinja = id => {
+    console.log(id);
   };
   render() {
     return (
       <div className="App">
         <h1>My first React app </h1>
         <p>Welcome: )</p>
-        <Ninjas ninjas={this.state.ninjas} />
+
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
   }
